b1b73d613be602249f4d3ac48b86bd46b221fd6b
kauedelazzeri <kdelazzeri@weg.net>
2022-11-22 21:47:44 -0300

    inicio de arquitetura, tentativa falha de pegar xml e usar svm

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import camelot
+import numpy as np
+import pandas as pd
+from sklearn import svm
 
 # PDF file to extract tables from
-file = "sample.pdf"
+file = "Cardapio\sample.pdf"
 
 # extract all the tables in the PDF file
 tables = camelot.read_pdf(file)
@@ -11,3 +14,18 @@ print("Total tables extracted:", tables.n)
 
 # print the first table as Pandas DataFrame
 print(tables[0].df)
+
+# convert to array
+data = []
+for i in range(tables.n):
+    data.append(tables[i].df.to_numpy())
+
+# Remove comments
+train = data[0:4]
+test = data[5]
+# svm train
+y = [0, 0, 1, 1]
+clf = svm.SVC()
+clf.fit(train, y)
+
+print(np.round(np.clip(clf.predict(test), 0, 1)).astype(bool))  # binary
