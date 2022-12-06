95703d62abd04ca3d7e4ef8b744e7dc993ae20f9
kauedelazzeri <kdelazzeri@weg.net>
2022-12-06 20:57:59 -0300

    criacao de classes sem padrao nenhum e uso da main pra fingir que tem algo organizado

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
-import camelot
-import numpy as np
-import pandas as pd
-from sklearn import svm
+from xml2df import Converter
 
-# PDF file to extract tables from
-file = "Cardapio\sample.pdf"
+class Converter_xml2df:
 
-# extract all the tables in the PDF file
-tables = camelot.read_pdf(file)
-
-# number of tables extracted
-print("Total tables extracted:", tables.n)
-
-# print the first table as Pandas DataFrame
-print(tables[0].df)
-
-# convert to array
-data = []
-for i in range(tables.n):
-    data.append(tables[i].df.to_numpy())
-
-# Remove comments
-train = data[0:4]
-test = data[5]
-# svm train
-y = [0, 0, 1, 1]
-clf = svm.SVC()
-clf.fit(train, y)
-
-print(np.round(np.clip(clf.predict(test), 0, 1)).astype(bool))  # binary
+    cardapio = Converter("items7")
+    cardapio.find_file()
+    cardapio.find_titles()
+    cardapio.save_df()
+    cardapio.sort_df()
+    cardapio.sums_calories()
+    cardapio.save_csv()
