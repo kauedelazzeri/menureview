0e922b19095560be157f34a632731dffb8c78a38
kauedelazzeri <kdelazzeri@weg.net>
2023-02-07 19:59:23 -0300

    1 voto por dia, problema para voltar ao inicio para votar, voto não é salvo

--- front.py
+++ front.py
@@ -1,6 +1,12 @@
 from flask import Flask, request
 import csv
 
+from jinja2 import Template
+
+template = Template(open('template.html').read())
+rendered_template = template.render()
+#print(rendered_template)
+
 app = Flask(__name__)
 filename = 'items2_10-12-2022 201016.csv'
 type = 'Gerado'
@@ -34,17 +40,22 @@ def index():
                 <h2>Day {}</h2>
                 """.format(week_day)
 
+        response += """
+                <p>Menu items for the day:</p>
+                """
         for item in food_items:
             response += """
                 <p>{}</p>
-                <label for="{}_good">Good</label>
-                <input type="radio" id="{}_good" name="{}" value="good">
-                <label for="{}_bad">Bad</label>
-                <input type="radio" id="{}_bad" name="{}" value="bad">
-                <br>
-                """.format(item["title"], item["title"], item["title"], item["title"], item["title"], item["title"], item["title"])
+                """.format(item["title"])
 
         response += """
+                <br>
+                <label for="vote">Your Vote:</label><br>
+                <input type="radio" id="vote_good" name="vote" value="good">
+                <label for="vote_good">Good</label><br>
+                <input type="radio" id="vote_bad" name="vote" value="bad">
+                <label for="vote_bad">Bad</label><br>
+                <br>
                 <input type="submit" value="Submit">
             </form>
         </body>
@@ -58,16 +69,23 @@ def index():
             reader = csv.DictReader(file)
             data = [row for row in reader]
 
-        # Update the vote in the CSV file
+        # Check if the day has already been voted on
         week_day = None
         for item in data:
-            if request.form.get(item["title"]):
-                week_day = item["week_day"]
-                item["vote"] = request.form[item["title"]]
-                break
-
+            if "title" in item:
+                vote = request.form.get(item["title"])
+                if vote:
+                    week_day = item["week_day"]
+                    break
         if week_day is None:
             return "No vote was received. Please try again."
+        if week_day in [item["week_day"] for item in data if "vote" in item]:
+            return "The vote for day {} has already been submitted. Thank you for your participation.".format(week_day)
+
+        # Update the vote in the CSV file
+        for item in data:
+            if item["week_day"] == week_day:
+                item["vote"] = vote
 
         with open('Cardapio\\Votado\\'+str(filename), "w") as file:
             writer = csv.DictWriter(file, fieldnames=["week_day", "title", "k_calories", "category", "vote"])
@@ -75,6 +93,8 @@ def index():
             writer.writerows(data)
 
         return "Thanks for your vote for day {}!".format(week_day)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
